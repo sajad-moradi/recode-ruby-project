9b8ff60801c6e3e0592000d4f61648727c791e66
sajad-moradi <sajad.moradi2006.com>
2019-12-06 23:52:36 +0800

    Exercise 7 - Simple If(case statement)

---
+++ exercise_7b.rb
@@ -0,0 +1,19 @@
+puts "Please choose a number between 0 and 100!"
+x = gets.chomp.to_i
+
+# if x >= 0 && x <= 50
+#   puts "The number that you chose (#{x}) is between 0 and 50!"
+# elsif x >= 51 && x <= 100
+#   puts "The number that you chose (#{x}) is between 51 and 100!"
+# else
+#   puts "The number that you chose (#{x}) is above 100!"
+# end
+
+case number_value
+when x >= 0 && x <= 50
+  puts "The number that you chose (#{x}) is between 0 and 50!"
+when x >= 51 && x <= 100
+  puts "The number that you chose (#{x}) is between 51 and 100!"
+else
+  puts "The number that you chose (#{x}) is above 100!"
+end
