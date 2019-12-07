c27895cead2aa0f781b89526a79c4ec67fd23a43
sajad-moradi <sajad.moradi2006.com>
2019-12-07 21:16:27 +0800

    Exercise 14 - Multiplication Table

---
+++ exercise_14.rb
@@ -0,0 +1,26 @@
+puts "Please enter the number."
+input_number = gets.chomp
+if input_number != input_number.to_i.to_s
+  print "Wrong input!"
+else
+  input_number = input_number.to_i
+  for number in 1..13
+    print "*"
+  end
+  puts ""
+  for i in 1..input_number
+    print "*"
+    for number in 1..input_number
+      if i * number > 9
+        print "#{i * number}*"
+      else
+        print " #{i * number}*"
+      end
+    end
+    puts ""
+    for number in 1..13
+      print "*"
+    end
+    puts ""
+  end
+end
