a3cbc295815973a6c8e79b4b057381488236ded9
sajad-moradi <sajad.moradi2006.com>
2019-12-22 21:52:32 +0800

    Resubmit Exam

--- exam_12.rb
+++ exam_12.rb
@@ -5,9 +5,14 @@
 # Remember that lowercase and uppercase characters have different values
 
 def caesar_cipher(string, shift_factor)
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    string.gsub(/\w/, "#{alphabet[alphabet.index(\w)+shift_factor]}")
-    return string
+  alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  return string.split(//).map { |el|
+           el.gsub(/\w/, "#{ind = alphabet.index(el).to_i + shift_factor
+             until alphabet.index(el).to_i > 25 && ind < 52 || alphabet.index(el).to_i < 26 && ind < 26
+             ind -= 26
+           end
+             alphabet[ind]}")
+         }.join
 end
 
 print caesar_cipher("What a string!", 5) == "Bmfy f xywnsl!"
